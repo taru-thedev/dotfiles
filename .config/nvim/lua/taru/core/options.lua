6bb17f83967451fd016c13312fafaf0e89f4be68
taru-thedev <tarunbhandari.work@gmail.com>
2025-06-15 19:46:38 +0530

    add basic config

--- .config/nvim/lua/taru/core/options.lua
+++ .config/nvim/lua/taru/core/options.lua
@@ -1,3 +1,17 @@
 vim.opt.guicursor = ""
+
 vim.opt.nu = true
 vim.opt.rnu = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undofile = true
+
