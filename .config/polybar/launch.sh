6d4fcce18cc76b4c7a9a8ee8e084ca3f6256297e
taru-thedev <tarunbhandari.work@gmail.com>
2025-06-21 18:47:40 +0530

    modify the script

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,11 +4,8 @@
 # Terminate already running bar instances
 killall -q polybar
 
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar example &
+polybar --config=/home/tarunbhandari/.config/polybar/config.ini systray_bar &
 
 echo "Polybar launched..."
 
